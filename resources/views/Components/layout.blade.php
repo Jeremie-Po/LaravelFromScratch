9dd7df7d58eb219c0bf49be22a92588103d0a63a
Jérémie P <jpourageaux@hotmail.fr>
2025-01-30 18:55:42 +0100

    add new button component

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -119,8 +119,13 @@
         </nav>
 
         <header class="bg-white shadow-sm">
-            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 flex justify-between">
                 <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $heading }}</h1>
+                <x-button href="/jobs/create">
+                    New Job
+                </x-button>
+            </div>
+            <div>
             </div>
         </header>
         <main>
