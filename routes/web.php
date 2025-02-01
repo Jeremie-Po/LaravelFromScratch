aa6a87ad8e5368e006a9322e1924a78a53148822
Jérémie P <jpourageaux@hotmail.fr>
2025-02-01 12:48:56 +0100

    refactor routes with route::resource method

--- routes/web.php
+++ routes/web.php
@@ -7,15 +7,17 @@ Route::view('/', 'home');
 Route::view('/about', 'about');
 Route::view('/contact', 'contact');
 
-Route::controller(jobController::class)->group(function () {
-    Route::get('/jobs', 'index');
-    Route::get('/jobs/{job}', 'show');
-    Route::get('/jobs/create', 'create');
-    Route::post('/jobs', 'store');
-    Route::get('/jobs/{job}/edit', 'edit');
-    Route::patch('/jobs/{job}', 'update');
-    Route::delete('/jobs/{job}', 'destroy');
-});
+//Route::controller(jobController::class)->group(function () {
+//    Route::get('/jobs', 'index');
+//    Route::get('/jobs/{job}', 'show');
+//    Route::get('/jobs/create', 'create');
+//    Route::post('/jobs', 'store');
+//    Route::get('/jobs/{job}/edit', 'edit');
+//    Route::patch('/jobs/{job}', 'update');
+//    Route::delete('/jobs/{job}', 'destroy');
+//});
+
+Route::resource('jobs', jobController::class);
 
 
 
