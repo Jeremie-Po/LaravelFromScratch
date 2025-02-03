67e2c50e3ef2722f7a339e3fbe862462e42f0181
Jérémie P <jpourageaux@hotmail.fr>
2025-02-03 12:16:30 +0100

    create register create method

--- app/Http/Controllers/authController.php
+++ app/Http/Controllers/authController.php
@@ -2,14 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
+
 class authController extends Controller
 {
     public function create(){
+
         return view('auth.register');
     }
 
     public function store()
     {
-        dd('hello');
+        $attrs = request()->validate([
+            'firstName' => ['required', 'string', 'max:255'],
+            'lastName' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', Password::min(6), 'confirmed']
+        ]);
+
+        $user = User::create($attrs);
+
+        Auth::login($user);
+
+        return redirect('/jobs');
     }
+
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,23 +8,23 @@
             <div class="border-b border-gray-900/10 pb-12">
                 <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                     <x-form-field>
-                        <x-form-label for="first_name">First name</x-form-label>
+                        <x-form-label for="firstName">First name</x-form-label>
 
                         <div class="mt-2">
-                            <x-form-input name="first_name" id="first_name"
+                            <x-form-input name="firstName" id="firstName"
                                           required/>
                         </div>
-                        <x-form-error name="first_name"/>
+                        <x-form-error name="firstName"/>
                     </x-form-field>
 
 
                     <x-form-field>
-                        <x-form-label for="last_name">Last name</x-form-label>
+                        <x-form-label for="lastName">Last name</x-form-label>
                         <div class="mt-2">
-                            <x-form-input name="last_name" id="last_name"
+                            <x-form-input name="lastName" id="lastName"
                                           required/>
                         </div>
-                        <x-form-error name="last_name"/>
+                        <x-form-error name="lastName"/>
                     </x-form-field>
 
                     <x-form-field>
@@ -48,7 +48,7 @@
                     <x-form-field>
                         <x-form-label for="password_confirmation">Password confirmation</x-form-label>
                         <div class="mt-2">
-                            <x-form-input type="password_confirmation" name="password_confirmation"
+                            <x-form-input type="password" name="password_confirmation"
                                           id="password_confirmation"
                                           required/>
                         </div>
