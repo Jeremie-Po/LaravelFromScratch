67886d2998e414f7be64ead262d7ba6878b80774
Jérémie P <jpourageaux@hotmail.fr>
2025-02-05 10:58:17 +0100

    send email when storing a new job

--- app/Http/Controllers/jobController.php
+++ app/Http/Controllers/jobController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\JobPosted;
 use App\Models\Job;
+use Illuminate\Support\Facades\Mail;
 
 class jobController extends Controller
 {
@@ -35,11 +37,13 @@ class jobController extends Controller
             'salary' => ['required', 'integer'],
         ]);
 
-        Job::create([
+        $job = Job::create([
             'title' => request('title'),
             'salary' => request('salary'),
             'employer_id' => 1,
         ]);
+
+        mail::to($job->employer->user)->send(new JobPosted($job));
 
         return redirect('/jobs');
     }
--- resources/views/mail/job-posted.blade.php
+++ resources/views/mail/job-posted.blade.php
@@ -1,3 +1,11 @@
-<div>
-    hello world
-</div>
+<h2>
+    {{ $job->title }}
+</h2>
+
+<p>
+    Congrats! Your job is now live on our website.
+</p>
+
+<p>
+    <a href="{{ url('/jobs/' . $job->id) }}">View Your Job Listing</a>
+</p>
